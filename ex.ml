9855acc828db01af59516f3fa4d3027ee700170b
Adrian Sampson <adrian@radbox.org>
2018-05-20 11:16:29 -0400

    eval function

--- ex.ml
+++ ex.ml
@@ -4,7 +4,11 @@ let pretty (e : expr) : string =
     match e with
     | Int i -> string_of_int i
 
+let eval (e : expr) : int =
+    match e with
+    | Int i -> i
+
 let _ =
     let lexbuf = Lexing.from_channel stdin in
     let expr = Parser.prog Lexer.token lexbuf in
-    print_endline (pretty expr)
+    print_endline (string_of_int (eval expr))
