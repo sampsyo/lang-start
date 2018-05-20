a458b5b49bbc603410284707f41b42fe9dc383f8
Adrian Sampson <adrian@radbox.org>
2018-05-20 11:15:29 -0400

    Pretty printer

--- ex.ml
+++ ex.ml
@@ -1,4 +1,10 @@
+open Ast
+
+let pretty (e : expr) : string =
+    match e with
+    | Int i -> string_of_int i
+
 let _ =
     let lexbuf = Lexing.from_channel stdin in
-    let result = Parser.prog Lexer.token lexbuf in
-    print_endline "parsed"
+    let expr = Parser.prog Lexer.token lexbuf in
+    print_endline (pretty expr)
